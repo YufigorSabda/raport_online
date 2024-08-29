f64d411f9c6485bc665f81ad83ec522a6c240a83
Gilang Raka Ramadhan <gilangrakar@gmail.com>
2024-08-29 10:32:55 +0700

    Menambah function update wali kelas

--- app/Http/Controllers/ManajemenKelas/WaliKelasController.php
+++ app/Http/Controllers/ManajemenKelas/WaliKelasController.php
@@ -18,4 +18,25 @@ class WaliKelasController extends Controller
 
         return view('backend.wali_kelas.index', compact('data'));
     }
+
+    public function update(Request $request, $id)
+    {
+        $validatedData = $request->validate([
+            'id' => ['required'],
+            'id_guru' => ['required', 'exists:ref_guru,id']
+        ]);
+        $ref_kelas = RefKelas::findOrFail($id);
+
+        try {
+            $ref_kelas->id_guru = $validatedData['id_guru'];
+            $ref_kelas->save();
+
+            $request->session()->flash('message', 'Berhasil mengedit wali kelas');
+            $request->session()->flash('alert-type', 'success');
+        } catch (\Exception $e) {
+            $request->session()->flash('message', 'Terjadi kesalahan. ' . $e->getMessage());
+            $request->session()->flash('alert-type', 'error');
+        }
+        return back();
+    }
 }
